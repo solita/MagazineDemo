263d63b9ea7739f0302b91206acd03b61843edf4
Jere Käpyaho <jere.kapyaho@solita.fi>
2020-11-30 11:46:44 +0200

    Initial Commit

---
+++ MagazineDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagazineDemo
+//
+//  Created by Jere Käpyaho on 30.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
